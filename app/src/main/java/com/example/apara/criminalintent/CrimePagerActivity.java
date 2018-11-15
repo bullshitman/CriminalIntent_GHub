702f21bfe4e995e0515fb37d5c234bcbe391fd63
Bullshitman <sokol.joe@gmail.com>
2018-11-15 23:52:08 +0300

    viewpager crime

--- app/src/main/java/com/example/apara/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/apara/criminalintent/CrimePagerActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -21,7 +21,7 @@ public class CrimePagerActivity extends AppCompatActivity {
         mViewPager = findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        mViewPager.setAdapter(new FragmentPagerAdapter() {
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
                 Crime crime = mCrimes.get(position);
